f94b0abd2ee3a6b1126853b15cfd78240af2dd58
Reemas Alnajjar <reemas9093@gmail.com>
2023-09-02 19:46:43 +0300

    reemas alnajjar CW4

---
+++ reemasaln4.py
@@ -0,0 +1,14 @@
+def my_name():
+    print("my name is Reemas")
+def my_meal(food, drink):
+    print(f"I like to eat {food} while drinking {drink}.")
+
+
+def cube(number):
+    return number**3
+def by_three(number):
+  if number%3 == 0:
+      return cube(number)
+  else:
+      return False
+  
